023db588dec0445b226900c4f4b9fe131cae0168
scott <scotthoang1996@gmail.com>
2019-10-15 09:43:57 -0500

    update download.py to retrieve correct data

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ import requests
 import os
 import tqdm
 import zipfile
-
+import json
 
 def download(arg):
 	"""download file at url and save at dir """
@@ -16,24 +16,42 @@ def download(arg):
 	os.remove(directory)
 
 
+def get_download_links(path):
+	"""obtain all download links from fdc's provided download json"""
+	with open(path, 'r') as handle:
+		file = json.load(handle)
+	drugs = file['results']["drug"]
+	links = []
+	for k in drugs.keys():
+		sub_section = drugs[k]
+		for p in sub_section['partitions']:
+			links.append(p['file'])
+	return links
+
+
 if __name__ == "__main__":
-
-	root_path = os.path.join("drug", "event")
-
-	if not os.path.isdir("drug"):
-		os.mkdir("drug")
-		os.mkdir(root_path)
+	links = get_download_links("download.json")
 	args = []
-	with open("file_path.txt", 'r') as f:
-		for url in f.readlines():
-			*_, time, filename = url.split("/")
-			filename = filename.strip("\n")
-			url = url.strip("\n")
-			sub_dir = os.path.join(root_path, time)
-			if not os.path.isdir(sub_dir):
-				os.mkdir(sub_dir)
-			args.append([url, os.path.join(sub_dir, filename)])
-			# download(url, os.path.join(sub_dir, filename))
+	for url in links:
+		infos = url.split("/")
+		infos = infos[3:] # getting rid of https and website
+		# now we reconstruct the path
+		path = ''
+		for i in infos:
+			path = os.path.join(path, i)
+			if i.endswith('.zip'):
+				break
+			else:
+				if not os.path.exists(path):
+					os.mkdir(path)
+		path = path.strip("\n")
+		# filename = filename.strip("\n")
+		url = url.strip("\n")
+		# sub_dir = os.path.join(root_path, time)
+		# if not os.path.isdir(sub_dir):
+		# 	os.mkdir(sub_dir)
+		args.append([url, path])
+		# download(url, os.path.join(sub_dir, filename))
 	p = Pool()
 	list(tqdm.tqdm(p.imap(download, args), ascii=True, total=len(args)))
 
