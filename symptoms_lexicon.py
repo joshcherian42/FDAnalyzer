1ea06063a34de1e3b90c4d94ccc719dd0d80edea
scott <scotthoang1996@gmail.com>
2019-10-27 16:25:46 -0500

    convert tab -> space

--- symptoms_lexicon.py
+++ symptoms_lexicon.py
@@ -5,33 +5,33 @@ from multiprocessing import Pool
 from itertools import chain
 
 def process(file_path):
-	symptoms = set()
-	with open(file_path, 'r') as handle:
-		data = json.load(handle)
-		events = data['results']
-		for event in events:
-			try:
-				reactions = event['patient']['reaction']
-				for reaction in reactions:
-					symptoms.add(reaction['reactionmeddrapt'].lower())
-			except KeyError:
-				continue
-	return symptoms
+    symptoms = set()
+    with open(file_path, 'r') as handle:
+        data = json.load(handle)
+        events = data['results']
+        for event in events:
+            try:
+                reactions = event['patient']['reaction']
+                for reaction in reactions:
+                    symptoms.add(reaction['reactionmeddrapt'].lower())
+            except KeyError:
+                continue
+    return symptoms
 
 if __name__ == "__main__":
-	root_path = os.path.join('drug', 'event')
-	files = []
+    root_path = os.path.join('drug', 'event')
+    files = []
 
-	for dir in os.listdir(root_path):
-		curr_path = os.path.join(root_path, dir)
-		for file in os.listdir(curr_path):
-			files.append(os.path.join(curr_path, file))
+    for dir in os.listdir(root_path):
+        curr_path = os.path.join(root_path, dir)
+        for file in os.listdir(curr_path):
+            files.append(os.path.join(curr_path, file))
 
-	p = Pool()
-	results = list(tqdm.tqdm(p.imap(process, files), total=len(files)))
-	p.close()
-	p.join()
-	results = set().union(*results)
-	lexicon = {k:i for i,k in enumerate(results)}
-	with open("symptoms.json", 'w') as handle:
-		json.dump(lexicon, handle)
+    p = Pool()
+    results = list(tqdm.tqdm(p.imap(process, files), total=len(files)))
+    p.close()
+    p.join()
+    results = set().union(*results)
+    lexicon = {k:i for i,k in enumerate(results)}
+    with open("symptoms.json", 'w') as handle:
+        json.dump(lexicon, handle)
